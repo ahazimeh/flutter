69dcc8b22b0e9ef452a809a3f17638aafcd20065
ahazimeh <hazimeh95@gmail.com>
2022-12-19 16:54:24 +0200

    Installing External Packages & Formatting Dates

--- ch-4/expense_planner/lib/main.dart
+++ ch-4/expense_planner/lib/main.dart
@@ -1,5 +1,6 @@
 import 'package:expense_planner/transaction.dart';
 import 'package:flutter/material.dart';
+import 'package:intl/intl.dart';
 
 void main() => runApp(const MyApp());
 
@@ -88,7 +89,8 @@ class MyHomePage extends StatelessWidget {
                         ),
                       ),
                       Text(
-                        tx.date.toString(),
+                        DateFormat.yMMMd().format(tx.date),
+                        // DateFormat('yyyy-MM-dd').format(tx.date),
                         style: const TextStyle(
                           color: Colors.grey,
                         ),
