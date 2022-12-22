f0d70bfadb027f1fbc16767f9077fe26b92c336b
ahazimeh <hazimeh95@gmail.com>
2022-12-22 12:21:07 +0200

    deriving recent transactions

--- ch-4/expense_planner/lib/main.dart
+++ ch-4/expense_planner/lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:expense_planner/models/transaction.dart';
+import 'package:expense_planner/widgets/chart.dart';
 import 'package:expense_planner/widgets/new_transaction.dart';
 import 'package:expense_planner/widgets/transaction_list.dart';
 import 'package:flutter/material.dart';
@@ -75,6 +76,12 @@ class _MyHomePageState extends State<MyHomePage> {
     // )
   ];
 
+  List<Transaction> get _recentTransactions {
+    return _userTransaction.where((tx) {
+      return tx.date.isAfter(DateTime.now().subtract(Duration(days: 7)));
+    }).toList();
+  }
+
   void _addNewTransaction(String title, double amount) {
     final newTx = Transaction(
       id: DateTime.now().toString(),
@@ -118,14 +125,7 @@ class _MyHomePageState extends State<MyHomePage> {
           // mainAxisAlignment: MainAxisAlignment.start,
           crossAxisAlignment: CrossAxisAlignment.stretch,
           children: <Widget>[
-            Container(
-              width: double.infinity,
-              child: const Card(
-                color: Colors.blue,
-                elevation: 5,
-                child: Text('CHART!'),
-              ),
-            ),
+            Chart(_recentTransactions),
             TransactionList(_userTransaction),
           ],
         ),
---
+++ ch-4/expense_planner/lib/widgets/chart.dart
@@ -0,0 +1,43 @@
+// import 'dart:js_util';
+import 'package:intl/intl.dart';
+
+import 'package:expense_planner/models/transaction.dart';
+import 'package:flutter/material.dart';
+
+class Chart extends StatelessWidget {
+  final List<Transaction> recentTransactions;
+
+  const Chart(this.recentTransactions, {super.key});
+  List<Map<String, Object>> get groupedTransactionValues {
+    return List.generate(7, (index) {
+      final weekday = DateTime.now().subtract(
+        Duration(days: index),
+      );
+      var totalSum = 0.0;
+
+      for (var i = 0; i < recentTransactions.length; i++) {
+        if (recentTransactions[i].date.day == weekday.day &&
+            recentTransactions[i].date.month == weekday.month &&
+            recentTransactions[i].date.year == weekday.year) {
+          totalSum += recentTransactions[i].amount;
+        }
+      }
+      print(DateFormat.E().format(weekday));
+      print(totalSum);
+
+      return {'day': DateFormat.E().format(weekday), 'amount': totalSum};
+    });
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    print(groupedTransactionValues);
+    return Card(
+      elevation: 6,
+      margin: EdgeInsets.all(10),
+      child: Row(
+        children: [],
+      ),
+    );
+  }
+}
