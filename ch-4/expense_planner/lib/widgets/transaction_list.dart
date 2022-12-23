d379d4cb3dce3abb53c68186a6377f6fa1bf326a
ahazimeh <hazimeh95@gmail.com>
2022-12-23 12:04:17 +0200

    Calculating Sizes Dynamically

--- ch-4/expense_planner/lib/widgets/transaction_list.dart
+++ ch-4/expense_planner/lib/widgets/transaction_list.dart
@@ -11,60 +11,57 @@ class TransactionList extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Container(
-      height: 300,
-      child: transaction.isEmpty
-          ? Column(
-              children: [
-                Text(
-                  'No transactions added yet',
-                  style: Theme.of(context).textTheme.headline6,
+    return transaction.isEmpty
+        ? Column(
+            children: [
+              Text(
+                'No transactions added yet',
+                style: Theme.of(context).textTheme.headline6,
+              ),
+              SizedBox(
+                height: 10,
+              ),
+              Container(
+                height: 200,
+                child: Image.asset(
+                  'assets/images/waiting.png',
+                  fit: BoxFit.cover,
                 ),
-                SizedBox(
-                  height: 10,
-                ),
-                Container(
-                  height: 200,
-                  child: Image.asset(
-                    'assets/images/waiting.png',
-                    fit: BoxFit.cover,
-                  ),
-                )
-              ],
-            )
-          : ListView.builder(
-              itemBuilder: (ctx, index) {
-                return Card(
-                  elevation: 5,
-                  margin: EdgeInsets.symmetric(vertical: 8, horizontal: 5),
-                  child: ListTile(
-                    leading: CircleAvatar(
-                      backgroundColor: Colors.purple,
-                      foregroundColor: Colors.white,
-                      radius: 30,
-                      child: Padding(
-                        padding: EdgeInsets.all(6),
-                        child: FittedBox(
-                            child: Text('\$${transaction[index].amount}')),
-                      ),
-                    ),
-                    title: Text(
-                      transaction[index].title,
-                      style: Theme.of(context).textTheme.headline6,
-                    ),
-                    subtitle: Text(
-                      DateFormat.yMMMd().format(transaction[index].date),
-                    ),
-                    trailing: IconButton(
-                      onPressed: () => deleteTx(transaction[index].id),
-                      icon: Icon(Icons.delete),
-                      color: Theme.of(context).errorColor,
+              )
+            ],
+          )
+        : ListView.builder(
+            itemBuilder: (ctx, index) {
+              return Card(
+                elevation: 5,
+                margin: EdgeInsets.symmetric(vertical: 8, horizontal: 5),
+                child: ListTile(
+                  leading: CircleAvatar(
+                    backgroundColor: Colors.purple,
+                    foregroundColor: Colors.white,
+                    radius: 30,
+                    child: Padding(
+                      padding: EdgeInsets.all(6),
+                      child: FittedBox(
+                          child: Text('\$${transaction[index].amount}')),
                     ),
                   ),
-                );
-              },
-              itemCount: transaction.length,
-            ),
-    );
+                  title: Text(
+                    transaction[index].title,
+                    style: Theme.of(context).textTheme.headline6,
+                  ),
+                  subtitle: Text(
+                    DateFormat.yMMMd().format(transaction[index].date),
+                  ),
+                  trailing: IconButton(
+                    onPressed: () => deleteTx(transaction[index].id),
+                    icon: Icon(Icons.delete),
+                    color: Theme.of(context).errorColor,
+                  ),
+                ),
+              );
+            },
+            itemCount: transaction.length,
+          );
   }
 }
