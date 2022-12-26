09484cc4b27d1259b2afa5b577487cd201315076
ahazimeh <hazimeh95@gmail.com>
2022-12-26 15:08:31 +0200

    Adding Links to the Drawer

---
+++ ch-7/meals_app/lib/screens/filters_screen.dart
@@ -0,0 +1,13 @@
+import 'package:flutter/material.dart';
+
+class FiltersScreen extends StatelessWidget {
+  static const routeName = '/filters';
+  const FiltersScreen({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Center(
+      child: Text('Filters!'),
+    );
+  }
+}
--- ch-7/meals_app/lib/widgets/main_drawer.dart
+++ ch-7/meals_app/lib/widgets/main_drawer.dart
@@ -1,7 +1,8 @@
 import 'package:flutter/material.dart';
+import 'package:meals_app/screens/filters_screen.dart';
 
 class MainDrawer extends StatelessWidget {
-  Widget buildListTile(String title, IconData icon) {
+  Widget buildListTile(String title, IconData icon, VoidCallback tapHandler) {
     return ListTile(
       leading: Icon(
         icon,
@@ -15,7 +16,7 @@ class MainDrawer extends StatelessWidget {
           fontWeight: FontWeight.bold,
         ),
       ),
-      onTap: () {},
+      onTap: tapHandler,
     );
   }
 
@@ -41,14 +42,12 @@ class MainDrawer extends StatelessWidget {
             ),
           ),
           const SizedBox(height: 20),
-          buildListTile(
-            'Meals',
-            Icons.restaurant,
-          ),
-          buildListTile(
-            'Filters',
-            Icons.settings,
-          )
+          buildListTile('Meals', Icons.restaurant, () {
+            Navigator.of(context).pushNamed('/');
+          }),
+          buildListTile('Filters', Icons.settings, () {
+            Navigator.of(context).pushNamed(FiltersScreen.routeName);
+          })
         ],
       ),
     );
