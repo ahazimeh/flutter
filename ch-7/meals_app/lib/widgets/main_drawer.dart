ad97e7d80f54946209e3a890730519902f682e2b
ahazimeh <hazimeh95@gmail.com>
2022-12-26 15:15:25 +0200

    Replacing Pages (Instead of Pushing)

--- ch-7/meals_app/lib/widgets/main_drawer.dart
+++ ch-7/meals_app/lib/widgets/main_drawer.dart
@@ -28,25 +28,24 @@ class MainDrawer extends StatelessWidget {
       child: Column(
         children: [
           Container(
-            height: 120,
-            width: double.infinity,
-            padding: const EdgeInsets.all(20),
-            alignment: Alignment.centerLeft,
-            color: Theme.of(context).colorScheme.secondary,
-            child: Text(
-              'Cooking Up!',
-              style: TextStyle(
-                  fontWeight: FontWeight.w900,
-                  fontSize: 30,
-                  color: Theme.of(context).colorScheme.primary),
-            ),
-          ),
+              height: 120,
+              width: double.infinity,
+              padding: const EdgeInsets.all(20),
+              alignment: Alignment.centerLeft,
+              color: Theme.of(context).colorScheme.secondary,
+              child: Text(
+                'Cooking Up!',
+                style: TextStyle(
+                    fontWeight: FontWeight.w900,
+                    fontSize: 30,
+                    color: Theme.of(context).colorScheme.primary),
+              )),
           const SizedBox(height: 20),
           buildListTile('Meals', Icons.restaurant, () {
-            Navigator.of(context).pushNamed('/');
+            Navigator.of(context).pushReplacementNamed('/');
           }),
           buildListTile('Filters', Icons.settings, () {
-            Navigator.of(context).pushNamed(FiltersScreen.routeName);
+            Navigator.of(context).pushReplacementNamed(FiltersScreen.routeName);
           })
         ],
       ),
