e906d18a64ea36338bda07e274c54d30d71bc453
ahazimeh <hazimeh95@gmail.com>
2022-12-26 11:36:38 +0200

    Adding a TabBar to the Appbar

--- ch-7/meals_app/lib/main.dart
+++ ch-7/meals_app/lib/main.dart
@@ -2,6 +2,7 @@ import 'package:flutter/material.dart';
 import 'package:meals_app/screens/categories_screen.dart';
 import 'package:meals_app/screens/category_meals_screen.dart';
 import 'package:meals_app/screens/meal_detail_screen.dart';
+import 'package:meals_app/screens/tabs_screen.dart';
 
 void main() {
   runApp(const MyApp());
@@ -36,7 +37,8 @@ class MyApp extends StatelessWidget {
       // home: const CategoriesScreen(),
       initialRoute: '/', // default
       routes: {
-        '/': (ctx) => const CategoriesScreen(),
+        // '/': (ctx) => const CategoriesScreen(),
+        '/': (ctx) => const TabsScreen(),
         CategoryMealsScreen.routeName: (ctx) => const CategoryMealsScreen(),
         MealDetailScreen.routeName: (ctx) => const MealDetailScreen(),
       },
--- ch-7/meals_app/lib/screens/categories_screen.dart
+++ ch-7/meals_app/lib/screens/categories_screen.dart
@@ -7,24 +7,21 @@ class CategoriesScreen extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(title: const Text('DeliMeal')),
-      body: GridView(
-        padding: const EdgeInsets.all(25),
-        gridDelegate: const SliverGridDelegateWithMaxCrossAxisExtent(
-          maxCrossAxisExtent: 200,
-          childAspectRatio: 3 / 2,
-          crossAxisSpacing: 20,
-          mainAxisSpacing: 20,
-        ),
-        children: DUMMY_CATEGORIES.map((catData) {
-          return CategoryItem(
-            catData.id,
-            catData.title,
-            catData.color,
-          );
-        }).toList(),
+    return GridView(
+      padding: const EdgeInsets.all(25),
+      gridDelegate: const SliverGridDelegateWithMaxCrossAxisExtent(
+        maxCrossAxisExtent: 200,
+        childAspectRatio: 3 / 2,
+        crossAxisSpacing: 20,
+        mainAxisSpacing: 20,
       ),
+      children: DUMMY_CATEGORIES.map((catData) {
+        return CategoryItem(
+          catData.id,
+          catData.title,
+          catData.color,
+        );
+      }).toList(),
     );
   }
 }
