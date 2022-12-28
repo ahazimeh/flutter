3480b2a4666262d5bbbdfee1d0d7bf5a2a85f336
ahazimeh <hazimeh95@gmail.com>
2022-12-28 23:56:15 +0200

    Displaying a List of Cart Items

--- ch-8/shop_app/lib/screens/cart_screen.dart
+++ ch-8/shop_app/lib/screens/cart_screen.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:provider/provider.dart';
-import 'package:shop_app/providers/cart.dart';
+import 'package:shop_app/providers/cart.dart' show Cart;
+import 'package:shop_app/widgets/cart_item.dart';
 
 class CartScreen extends StatelessWidget {
   static const routeName = '/cart';
@@ -13,43 +14,58 @@ class CartScreen extends StatelessWidget {
       appBar: AppBar(
         title: Text('Your Cart'),
       ),
-      body: Column(children: [
-        Card(
-          margin: EdgeInsets.all(15),
-          child: Padding(
-            padding: EdgeInsets.all(8),
-            child: Row(
-              mainAxisAlignment: MainAxisAlignment.spaceBetween,
-              children: [
-                Text(
-                  'Total',
-                  style: TextStyle(
-                    fontSize: 20,
-                  ),
-                ),
-                Spacer(),
-                Chip(
-                  label: Text(
-                    '\$${cart.totalAmount}',
+      body: Column(
+        children: [
+          Card(
+            margin: EdgeInsets.all(15),
+            child: Padding(
+              padding: EdgeInsets.all(8),
+              child: Row(
+                mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                children: [
+                  Text(
+                    'Total',
                     style: TextStyle(
-                        color: Theme.of(context)
-                            .primaryTextTheme
-                            .headline6
-                            ?.color),
+                      fontSize: 20,
+                    ),
                   ),
-                  backgroundColor: Theme.of(context).colorScheme.primary,
-                ),
-                TextButton(
-                  onPressed: () {},
-                  child: Text('ORDER NOW'),
-                  style: TextButton.styleFrom(
-                      foregroundColor: Theme.of(context).colorScheme.primary),
-                ),
-              ],
+                  Spacer(),
+                  Chip(
+                    label: Text(
+                      '\$${cart.totalAmount}',
+                      style: TextStyle(
+                          color: Theme.of(context)
+                              .primaryTextTheme
+                              .headline6
+                              ?.color),
+                    ),
+                    backgroundColor: Theme.of(context).colorScheme.primary,
+                  ),
+                  TextButton(
+                    onPressed: () {},
+                    child: Text('ORDER NOW'),
+                    style: TextButton.styleFrom(
+                        foregroundColor: Theme.of(context).colorScheme.primary),
+                  ),
+                ],
+              ),
             ),
           ),
-        ),
-      ]),
+          SizedBox(height: 10),
+          Expanded(
+              child: ListView.builder(
+            itemBuilder: (ctx, i) {
+              return CartItem(
+                cart.items.values.toList()[i].id,
+                cart.items.values.toList()[i].price,
+                cart.items.values.toList()[i].quantity,
+                cart.items.values.toList()[i].title,
+              );
+            },
+            itemCount: cart.itemCount,
+          )),
+        ],
+      ),
     );
   }
 }
