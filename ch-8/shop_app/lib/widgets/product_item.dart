90ca7578bb8546a87e820fef8f95c2efbf51f0c3
ahazimeh <hazimeh95@gmail.com>
2022-12-30 14:10:33 +0200

    Snackbars & Undoing Add to Cart Actions

--- ch-8/shop_app/lib/widgets/product_item.dart
+++ ch-8/shop_app/lib/widgets/product_item.dart
@@ -45,6 +45,22 @@ class ProductItem extends StatelessWidget {
                 product.price,
                 product.title,
               );
+              ScaffoldMessenger.of(context).hideCurrentSnackBar();
+              ScaffoldMessenger.of(context).showSnackBar(
+                SnackBar(
+                  content: const Text(
+                    'Added item to cart!',
+                  ),
+                  duration: const Duration(seconds: 10),
+                  action: SnackBarAction(
+                    label: 'UNDO',
+                    onPressed: () {
+                      cart.removeSingleItem(product.id);
+                      // ScaffoldMessenger.of(context).hideCurrentSnackBar();
+                    },
+                  ),
+                ),
+              );
             },
             color: Theme.of(context).colorScheme.secondary,
           ),
