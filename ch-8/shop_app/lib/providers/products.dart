f54656225448c813f79d0871c45e49921adbe6a1
ahazimeh01 <ali.h@echovalley.io>
2023-01-14 22:07:51 +0200

    async await

--- ch-8/shop_app/lib/providers/products.dart
+++ ch-8/shop_app/lib/providers/products.dart
@@ -66,22 +66,21 @@ class Products with ChangeNotifier {
   //   notifyListeners();
   // }
 
-  Future<void> addProduct(Product product) {
+  Future<void> addProduct(Product product) async {
     // final url = Uri.https('flutter-update.firebaseio.com', '/products.json');
     final url = Uri.parse(
-        'https://flutter-max-f1b24-default-rtdb.firebaseio.com/products');
-    return http
-        .post(
-      url,
-      body: json.encode({
-        'title': product.title,
-        'description': product.description,
-        'imageUrl': product.imageUrl,
-        'price': product.price,
-        'isFavorite': product.isFavorite,
-      }),
-    )
-        .then((response) {
+        'https://flutter-max-f1b24-default-rtdb.firebaseio.com/products.json');
+    try {
+      final response = await http.post(
+        url,
+        body: json.encode({
+          'title': product.title,
+          'description': product.description,
+          'imageUrl': product.imageUrl,
+          'price': product.price,
+          'isFavorite': product.isFavorite,
+        }),
+      );
       print(json.decode(response.body));
       final newProduct = Product(
         title: product.title,
@@ -93,9 +92,9 @@ class Products with ChangeNotifier {
       _items.add(newProduct);
       // _items.insert(0,newProduct);// at the start of the list
       notifyListeners();
-    }).catchError((error) {
+    } catch (error) {
       throw error;
-    });
+    }
   }
 
   void updateProduct(String id, Product newProduct) {
--- ch-8/shop_app/lib/screens/edit_product_screen.dart
+++ ch-8/shop_app/lib/screens/edit_product_screen.dart
@@ -88,7 +88,7 @@ class _EditProductScreenState extends State<EditProductScreen> {
     }
   }
 
-  void _saveForm() {
+  Future<void> _saveForm() async {
     final isValid = _form.currentState?.validate() ?? false;
     if (!isValid) {
       return;
@@ -105,10 +105,11 @@ class _EditProductScreenState extends State<EditProductScreen> {
       });
       Navigator.of(context).pop();
     } else {
-      Provider.of<Products>(context, listen: false)
-          .addProduct(_editedProduct)
-          .catchError((error) {
-        return showDialog<Null>(
+      try {
+        await Provider.of<Products>(context, listen: false)
+            .addProduct(_editedProduct);
+      } catch (error) {
+        await showDialog<Null>(
           context: context,
           builder: (ctx) => AlertDialog(
             title: Text('An error occured!'),
@@ -123,12 +124,12 @@ class _EditProductScreenState extends State<EditProductScreen> {
             ],
           ),
         );
-      }).then((_) {
+      } finally {
         setState(() {
           _isLoading = false;
         });
         Navigator.of(context).pop();
-      });
+      }
     }
   }
 
