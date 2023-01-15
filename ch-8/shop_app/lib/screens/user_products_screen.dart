fb6339e2e64745c88aa454eebf0af96c3a174d81
ahazimeh01 <ali.h@echovalley.io>
2023-01-15 20:44:30 +0200

    Implementing Pull-to-Refresh

--- ch-8/shop_app/lib/screens/user_products_screen.dart
+++ ch-8/shop_app/lib/screens/user_products_screen.dart
@@ -8,6 +8,10 @@ import 'package:shop_app/widgets/user_product_item.dart';
 class UserProductsScreen extends StatelessWidget {
   static const routeName = '/user-products';
 
+  Future<void> _refreshProducts(BuildContext context) async {
+    await Provider.of<Products>(context, listen: false).fetchAndSetProducts();
+  }
+
   const UserProductsScreen({super.key});
 
   @override
@@ -26,20 +30,23 @@ class UserProductsScreen extends StatelessWidget {
         ],
       ),
       drawer: const AppDrawer(),
-      body: Padding(
-        padding: const EdgeInsets.all(8),
-        child: ListView.builder(
-          itemBuilder: (_, i) => Column(
-            children: [
-              UserProductItem(
-                prodcutsData.items[i].id,
-                prodcutsData.items[i].title,
-                prodcutsData.items[i].imageUrl,
-              ),
-              Divider(),
-            ],
+      body: RefreshIndicator(
+        onRefresh: () => _refreshProducts(context),
+        child: Padding(
+          padding: const EdgeInsets.all(8),
+          child: ListView.builder(
+            itemBuilder: (_, i) => Column(
+              children: [
+                UserProductItem(
+                  prodcutsData.items[i].id,
+                  prodcutsData.items[i].title,
+                  prodcutsData.items[i].imageUrl,
+                ),
+                Divider(),
+              ],
+            ),
+            itemCount: prodcutsData.items.length,
           ),
-          itemCount: prodcutsData.items.length,
         ),
       ),
     );
