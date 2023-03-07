3e439c4c6527e0509faa293faa0144114e679733
ahazimeh01 <ali.h@echovalley.io>
2023-03-07 21:43:33 +0200

    Handling Authentication Errors

--- ch-8/shop_app/lib/providers/auth.dart
+++ ch-8/shop_app/lib/providers/auth.dart
@@ -2,6 +2,7 @@ import 'dart:convert';
 
 import 'package:flutter/material.dart';
 import 'package:http/http.dart' as http;
+import 'package:shop_app/models/http_exception.dart';
 
 class Auth with ChangeNotifier {
   String _token = "";
@@ -11,13 +12,20 @@ class Auth with ChangeNotifier {
   Future<void> _authenticate(String email, String password, urlSegment) async {
     final url = Uri.parse(
         'https://www.googleapis.com/identitytoolkit/v3/relyingparty/$urlSegment?key=AIzaSyCqm29QiHs31E_LIQjR84eu6YYA2aggNgE');
-    final response = await http.post(
-      url,
-      body: json.encode(
-        {'email': email, 'password': password, 'returnSecureToken': true},
-      ),
-    );
-    print(json.decode(response.body));
+    try {
+      final response = await http.post(
+        url,
+        body: json.encode(
+          {'email': email, 'password': password, 'returnSecureToken': true},
+        ),
+      );
+      final responseData = json.decode(response.body);
+      if (responseData['error'] != null) {
+        throw HttpException(responseData['error']['message']);
+      }
+    } catch (error) {
+      throw error;
+    }
   }
 
   Future<void> signup(String email, String password) async {
