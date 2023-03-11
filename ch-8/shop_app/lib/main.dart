01eec75444bc813aaee8fb658b3390e418c30004
ahazimeh01 <ali.h@echovalley.io>
2023-03-11 18:24:06 +0200

    section-12

--- ch-8/shop_app/lib/main.dart
+++ ch-8/shop_app/lib/main.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:provider/provider.dart';
+import 'package:shop_app/helpers/custom_route.dart';
 import 'package:shop_app/providers/auth.dart';
 import 'package:shop_app/providers/cart.dart';
 import 'package:shop_app/providers/orders.dart';
@@ -55,13 +56,16 @@ class MyApp extends StatelessWidget {
           builder: (ctx, auth, _) => MaterialApp(
             title: 'MyShop',
             theme: ThemeData(
-              colorScheme: ColorScheme.fromSwatch(
-                primarySwatch: Colors.purple,
-              ).copyWith(
-                secondary: Colors.deepOrange,
-              ),
-              fontFamily: 'Lato',
-            ),
+                colorScheme: ColorScheme.fromSwatch(
+                  primarySwatch: Colors.purple,
+                ).copyWith(
+                  secondary: Colors.deepOrange,
+                ),
+                fontFamily: 'Lato',
+                pageTransitionsTheme: PageTransitionsTheme(builders: {
+                  TargetPlatform.android: CustomPageTransitionBuilder(),
+                  TargetPlatform.iOS: CustomPageTransitionBuilder()
+                })),
             // home: ProductsOverviewScreen(),
             home: auth.isAuth
                 ? ProductsOverviewScreen()
