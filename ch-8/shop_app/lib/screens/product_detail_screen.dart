11a7592b65244e1ffc9cad9e1b5b3bc09d732ce6
ahazimeh01 <ali.h@echovalley.io>
2023-03-11 17:42:30 +0200

    adding a hero transition

--- ch-8/shop_app/lib/screens/product_detail_screen.dart
+++ ch-8/shop_app/lib/screens/product_detail_screen.dart
@@ -28,9 +28,12 @@ class ProductDetailScreen extends StatelessWidget {
             Container(
               width: double.infinity,
               height: 300,
-              child: Image.network(
-                loadedProduct.imageUrl,
-                fit: BoxFit.cover,
+              child: Hero(
+                tag: loadedProduct.id,
+                child: Image.network(
+                  loadedProduct.imageUrl,
+                  fit: BoxFit.cover,
+                ),
               ),
             ),
             SizedBox(
