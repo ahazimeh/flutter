6dee24eb2730f3a2ac1470c958a4cea28d3d8d25
ahazimeh01 <ali.h@echovalley.io>
2023-03-12 11:59:21 +0200

    Storing & Fetching Data with SQLite

--- ch-13/native_device_features/lib/providers/great_places.dart
+++ ch-13/native_device_features/lib/providers/great_places.dart
@@ -1,6 +1,7 @@
 import 'dart:io';
 
 import 'package:flutter/material.dart';
+import 'package:native_device_features/helpers/db_helpers.dart';
 import 'package:native_device_features/models/place.dart';
 
 class GreatPlaces with ChangeNotifier {
@@ -17,6 +18,26 @@ class GreatPlaces with ChangeNotifier {
         location: null,
         image: pickedImage);
     _items.add(newPlace);
+    DBHelper.insert('user_places', {
+      'id': newPlace.id,
+      'title': newPlace.title,
+      'image': newPlace.image.path,
+    });
+    notifyListeners();
+  }
+
+  Future<void> fetchAndSetPlaces() async {
+    final datalist = await DBHelper.getData('user_places');
+    _items = datalist
+        .map(
+          (item) => Place(
+            id: item['id'],
+            title: item['title'],
+            image: File(item['image']),
+            location: null,
+          ),
+        )
+        .toList();
     notifyListeners();
   }
 }
